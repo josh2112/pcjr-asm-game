0c43606386723049bea4807886a70cc9b0f8fe52
Joshua Foster <josh2112@gmail.com>
2016-09-13 14:07:53 -0400

    Initial commit of files for episode 3.

---
+++ Makefile
@@ -0,0 +1,36 @@
+#
+# Makefile for PCjr ASM Game project
+#
+ifeq ($(OS),Windows_NT)
+  NASM="$(USERPROFILE)\AppData\Local\NASM\nasm"
+  DOSBOX="$(ProgramFiles)\DOSBox-0.74\dosbox"
+  RM=cmd \/C del
+else
+  NASM=nasm
+  RM=rm
+  UNAME_S := $(shell uname -s)
+  ifeq ($(UNAME_S),Darwin)
+    DOSBOX=/Applications/DOSBox.app/Contents/MacOS/DOSBox
+  else # assume Linux
+    DOSBOX=DISPLAY=:0 dosbox
+  endif
+endif
+
+TARGET=test
+TARGET.COM=$(TARGET).com
+
+MACROS=stdio.mac
+SRCS=formatting.asm
+DEPS=$(MACROS) $(SRCS)
+
+NASM_OPTS=-f bin
+DOSBOX_OPTS=-conf dosbox.conf
+
+$(TARGET.COM): $(TARGET).asm $(DEPS)
+	$(NASM) $(NASM_OPTS) -o $@ $<
+
+run: $(TARGET.COM)
+	$(DOSBOX) $(DOSBOX_OPTS) $^
+
+clean:
+	$(RM) $(TARGET.COM)
--- test.asm
+++ test.asm
@@ -1,30 +1,19 @@
 [cpu 8086]
 [org 100h]
 
-mov ax, 65432         ; Load AX with the number we want to print
+jmp main
+
+%include 'formatting.asm'
+%include 'stdio.mac'
+
+main:
+
+mov ax, -1234         ; Load AX with the number we want to print
 mov di, buf16         ; Load DI with the address of an empty buffer
 
-mov bx, 10            ; Load divisor and clear counter
-mov cx, 0
-peelOffDigits:
-  xor dx, dx
-  div bx              ; This will give us the remainder in DX
-  push dx             ; Push it onto the stack
-  inc cl              ; Increment our counter
-  test ax, ax         ; If no result, we're done
-  jnz peelOffDigits
-buildString:
-  pop ax              ; Pop out the next digit
-  add al, '0'         ; Add 48 to get the ascii value
-  stosb               ; Store the char in AL into the location at
-                      ; [DI], then increment DI
-  loop buildString    ; Continue if we have more digits
+call int_to_string    ; Call our 'int-to-string' procedure to format the number
 
-mov byte [di], '$'  ; Terminate the string
-
-mov dx, buf16         ; Load DX with the address of the buffer
-mov ah, 9             ; Load AH with 9
-int 21h               ; Call INT21h fn 9 to print the buffer
+print buf16           ; Call our 'print' macro to print the formatted number
 
 mov ax, 4c00h         ; Call INT21h fn 0x4c to exit the program
 int 21h
