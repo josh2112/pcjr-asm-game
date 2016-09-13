e49b34c4bfca91b147efe7658b1c84e7cbfd9d6d
Joshua Foster <josh2112@gmail.com>
2016-09-13 11:55:53 -0400

    Initial commit of files for episode 2.

--- test.asm
+++ test.asm
@@ -1,11 +1,32 @@
 [cpu 8086]
 [org 100h]
 
-mov dx, str_msg
-mov ah, 9h
+mov ax, 65432         ; Load AX with the number we want to print
+mov di, buf16         ; Load DI with the address of an empty buffer
+
+mov bx, 10            ; Load divisor and clear counter
+mov cx, 0
+peelOffDigits:
+  xor dx, dx
+  div bx              ; This will give us the remainder in DX
+  push dx             ; Push it onto the stack
+  inc cl              ; Increment our counter
+  test ax, ax         ; If no result, we're done
+  jnz peelOffDigits
+buildString:
+  pop ax              ; Pop out the next digit
+  add al, '0'         ; Add 48 to get the ascii value
+  stosb               ; Store the char in AL into the location at
+                      ; [DI], then increment DI
+  loop buildString    ; Continue if we have more digits
+
+mov byte [di], '$'  ; Terminate the string
+
+mov dx, buf16         ; Load DX with the address of the buffer
+mov ah, 9             ; Load AH with 9
+int 21h               ; Call INT21h fn 9 to print the buffer
+
+mov ax, 4c00h         ; Call INT21h fn 0x4c to exit the program
 int 21h
 
-mov ax, 4c00h
-int 21h
-
-str_msg: db 'Hello, PCjr!', 0ah, 0dh, '$'
+buf16: times 16 db 0  ; An empty 16-byte buffer
