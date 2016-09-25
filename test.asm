7f4dfe0b19e5011ca166d4c9ffed4b883a5a3708
Joshua Foster <josh2112@gmail.com>
2016-09-25 14:45:59 -0400

    Draw player, process arrow keys

--- test.asm
+++ test.asm
@@ -10,7 +10,11 @@
 
 section .data
 
-str_crlf: db `\r\n$`
+str_crlf: db 0xa, 0xd, '$'
+
+is_running: db 1
+player_x: dw 160
+player_y: dw 100
 
 section .bss
 
@@ -30,20 +34,20 @@ mov [originalVideoMode], al  ; Store it into the byte pointed to by originalVide
 mov ax, 9h                   ; AH <- 0x00 (set video mode), AL <- 9 (new mode)
 int 10h                      ; Call INT10h fn 0 to change the video mode
 
-mov dl, 1
-call cls
+game_loop:
+  mov dl, 1
+  call cls
 
-;mov cx, 16
-;rotateColors:
-;  mov dl, 16
-;  sub dl, cl
-;  push cx
-;  call cls
-;  pop cx
-;  loop rotateColors
+  call draw_player
 
-; Call INT21h fn 8 (character input without echo) to wait for a keypress
-waitForAnyKey
+  xor ax, ax
+  int 16h
+  call process_key
+
+  cmp byte [is_running], 0
+  jne game_loop
+
+clean_up:
 
 ; Change the video mode back to whatever it was before (the value stored in
 ; originalVideoMode)
@@ -59,3 +63,53 @@ int 21h
 
 %include 'formatting.asm'
 %include '320x200x16.asm'
+
+process_key:
+  cmp ah, 1
+  jne .testUp
+  mov byte [is_running], 0
+  jmp .done
+.testUp:
+  cmp ah, 0x48
+  jne .testDown
+  dec word [player_y]
+  jmp .done
+.testDown:
+  cmp ah, 0x50
+  jne .testLeft
+  inc word [player_y]
+  jmp .done
+.testLeft:
+  cmp ah, 0x4b
+  jne .testRight
+  dec word [player_x]
+  jmp .done
+.testRight:
+  cmp ah, 0x4d
+  jne .done
+  inc word [player_x]
+.done:
+  ret
+
+draw_player:
+  mov cx, 8
+  .drawRow:
+    mov ax, 8
+    sub ax, cx
+    add ax, [player_y]      ; AX = row (y)
+    push cx
+    mov cx, 8
+    .drawPixel:
+      mov bx, 8
+      sub bx, cx
+      add bx, [player_x]    ; BX = col (x)
+      push ax
+      push cx
+      mov dl, 10
+      call putpixel         ; (BX, AX) = (x,y), DL = color
+      pop cx
+      pop ax
+      loop .drawPixel
+    pop cx
+    loop .drawRow
+  ret
