11a5adef18a8489e41965577f32cb77636aed142
Joshua Foster <josh2112@gmail.com>
2018-10-21 14:32:58 -0400

    - Added JMODEM.BAS

--- tools/txbin.py
+++ tools/txbin.py
@@ -3,17 +3,18 @@
 import serial
 import argparse
 
-url = "socket://localhost:7000"
-#url = "/dev/tty.UC232A" # or something like that
+#url = "socket://localhost:7000"
+url = "/dev/tty.UC-232AC" # or something like that
 
 parser = argparse.ArgumentParser( description='Sends a file over the serial port.' )
 parser.add_argument( 'path', help='path to the file to send' )
 args = parser.parse_args()
 
-with serial.serial_for_url( url ) as s:
-    print( f"Opened port {s.name}" )
+with serial.serial_for_url( url, baudrate=300 ) as s:
+    s.baudrate = 300
+    print( "Opened port {}".format( s.name ) )
     with open( args.path, 'rb' ) as f: data = f.read()
-    print( f"Sending file '{args.path}' ({len(data)} bytes)..." )
+    print( "Sending file '{}' ({} bytes)...".format( args.path, len(data)))
     s.write( data )
 
 print( "Done!" )
