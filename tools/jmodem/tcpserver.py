943b96aea601766409f25fee90628bf2a325eee9
Joshua Foster <josh2112@gmail.com>
2018-10-29 09:01:16 -0400

    - tcpserver.py - backward compatibility with Python 3.5  - JMODEM.BAS: Attempt to avoid "string too long" error when writing to a file

--- tools/jmodem/tcpserver.py
+++ tools/jmodem/tcpserver.py
@@ -12,21 +12,22 @@ addr_to_str = lambda addr: ':'.join( str(x) for x in addr[:2] )
 
 async def hub_server( reader, writer, clients, lock ):
     myaddr = writer.get_extra_info( 'peername' )
-    print( f"Client {addr_to_str(myaddr)} connected" )
+    print( "Client {} connected".format( addr_to_str(myaddr)))
     async with lock: clients[myaddr] = writer
     try:
         while True:
             data = await reader.read( 128 )
             if not data: break
+            print( "Got: ", data )
             async with lock:
                 destinations = {addr:writer for (addr,writer) in clients.items() if addr != myaddr}
                 if destinations:
-                    print( f"{len(data)} byte(s) {addr_to_str(myaddr)} => {[addr_to_str(x) for x in destinations.keys()]}" )
-                    for writer in destinations.values():
-                        writer.write( data )
+                    print( "{} byte(s) {} => {}".format( len(data), addr_to_str(myaddr), [addr_to_str(x) for x in destinations.keys()] ))
+                    for w in destinations.values():
+                        w.write( data )
     finally:
         async with lock: del clients[myaddr]
-        print( f"Client {addr_to_str(myaddr)} disconnected" )
+        print( "Client {} disconnected".format( addr_to_str(myaddr)))
 
 
 clients = {}
@@ -36,7 +37,7 @@ loop = asyncio.get_event_loop()
 coro = asyncio.start_server( lambda r,w: hub_server( r, w, clients, lock ), *hostaddr, loop=loop )
 server = loop.run_until_complete( coro )
 
-print( f'Serving on {addr_to_str(hostaddr)}' )
+print( "Serving on {}".format( addr_to_str(hostaddr) ))
 
 try: loop.run_forever()
 except KeyboardInterrupt: pass
